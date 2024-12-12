8e56e50bf42a8a79541b36ba54fd3654a625beec
Khalie <cymonmasindo0@gmail.com>
2024-12-11 14:39:20 -1000

    dnsuosv`

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,13 +1,12 @@
 package main;
 
-import java.awt.Graphics;
-
 import audio.AudioPlayer;
 import gamestates.Credits;
 import gamestates.GameOptions;
 import gamestates.Gamestate;
 import gamestates.Menu;
 import gamestates.Playing;
+import java.awt.Graphics;
 import ui.AudioOptions;
 
 public class Game implements Runnable {
@@ -26,8 +25,8 @@ public class Game implements Runnable {
 
 	public final static int TILES_DEFAULT_SIZE = 32;
 	public final static float SCALE = 2f;
-	public final static int TILES_IN_WIDTH = 26;
-	public final static int TILES_IN_HEIGHT = 14;
+	public final static int TILES_IN_WIDTH = 16;
+	public final static int TILES_IN_HEIGHT = 16;
 	public final static int TILES_SIZE = (int) (TILES_DEFAULT_SIZE * SCALE);
 	public final static int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
 	public final static int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -10,41 +10,41 @@ import javax.imageio.ImageIO;
 
 public class LoadSave {
     
-	public static final String PLAYER_ATLAS = ("/res/player.png");
-	public static final String LEVEL_ATLAS = ("/res/outside_sprites.png");
-	public static final String MENU_BUTTONS = ("/res/button_atlas.png");
-	public static final String MENU_BACKGROUND = ("/res/menu_background.png");
-	public static final String PAUSE_BACKGROUND = ("/res/pause_menu.png");
-	public static final String SOUND_BUTTONS = ("/res/sound_button.png");
-	public static final String URM_BUTTONS = ("/res/urm_buttons.png");
-	public static final String VOLUME_BUTTONS = ("/res/volume_buttons.png");
-	public static final String MENU_BACKGROUND_IMG = ("/res/background_menu.png");
-	public static final String PLAYING_BG_IMG = ("/res/playing_bg_img.png");
-	public static final String BIG_CLOUDS = ("/res/big_clouds.png");
-	public static final String SMALL_CLOUDS = ( "/res/small_clouds.png");
-	public static final String CRABBY_SPRITE = ("/res/crabby_sprite.png");
-	public static final String STATUS_BAR = ("/res/health_power_bar.png");
-	public static final String COMPLETED_IMG = ("/res/completed_sprite.png");
-	public static final String POTION_ATLAS = ("/res/potions_sprites.png");
-	public static final String CONTAINER_ATLAS = ("/res/objects_sprites.png");
-	public static final String TRAP_ATLAS = ("/res/trap_atlas.png");
-	public static final String CANNON_ATLAS = ("/res/cannon_atlas.png");
-	public static final String CANNON_BALL = ("/res/ball.png");
-	public static final String DEATH_SCREEN = ("/res/death_screen.png");
-	public static final String OPTIONS_MENU = ("/res/options_background.png");
-	public static final String PINKSTAR_ATLAS = ("/res/pinkstar_atlas.png");
-	public static final String QUESTION_ATLAS = ("/res/question_atlas.png");
-	public static final String EXCLAMATION_ATLAS = ("/res/exclamation_atlas.png");
-	public static final String SHARK_ATLAS = ("/res/shark_atlas.png");
-	public static final String CREDITS = ("/res/credits_list.png");
-	public static final String GRASS_ATLAS = ("/res/grass_atlas.png");
-	public static final String TREE_ONE_ATLAS = ("/res/tree_one_atlas.png");
-	public static final String TREE_TWO_ATLAS = ("/res/tree_two_atlas.png");
-	public static final String GAME_COMPLETED = ("/res/game_completed.png");
-	public static final String RAIN_PARTICLE = ("/res/rain_particle.png");
-	public static final String WATER_TOP = ("/res/water_atlas_animation.png");
-	public static final String WATER_BOTTOM = ("/res/water.png");
-	public static final String SHIP = ("/res/ship.png");
+	public static final String PLAYER_ATLAS = ("res/player.png");
+	public static final String LEVEL_ATLAS = ("res/outside_sprites.png");
+	public static final String MENU_BUTTONS = ("res/button_atlas.png");
+	public static final String MENU_BACKGROUND = ("res/menu_background.png");
+	public static final String PAUSE_BACKGROUND = ("res/pause_menu.png");
+	public static final String SOUND_BUTTONS = ("res/sound_button.png");
+	public static final String URM_BUTTONS = ("res/urm_buttons.png");
+	public static final String VOLUME_BUTTONS = ("res/volume_buttons.png");
+	public static final String MENU_BACKGROUND_IMG = ("res/background_menu.png");
+	public static final String PLAYING_BG_IMG = ("res/playing_bg_img.png");
+	public static final String BIG_CLOUDS = ("res/big_clouds.png");
+	public static final String SMALL_CLOUDS = ( "res/small_clouds.png");
+	public static final String CRABBY_SPRITE = ("res/crabby_sprite.png");
+	public static final String STATUS_BAR = ("res/health_power_bar.png");
+	public static final String COMPLETED_IMG = ("res/completed_sprite.png");
+	public static final String POTION_ATLAS = ("res/potions_sprites.png");
+	public static final String CONTAINER_ATLAS = ("res/objects_sprites.png");
+	public static final String TRAP_ATLAS = ("res/trap_atlas.png");
+	public static final String CANNON_ATLAS = ("res/cannon_atlas.png");
+	public static final String CANNON_BALL = ("res/ball.png");
+	public static final String DEATH_SCREEN = ("res/death_screen.png");
+	public static final String OPTIONS_MENU = ("res/options_background.png");
+	public static final String PINKSTAR_ATLAS = ("res/pinkstar_atlas.png");
+	public static final String QUESTION_ATLAS = ("res/question_atlas.png");
+	public static final String EXCLAMATION_ATLAS = ("res/exclamation_atlas.png");
+	public static final String SHARK_ATLAS = ("res/shark_atlas.png");
+	public static final String CREDITS = ("res/credits_list.png");
+	public static final String GRASS_ATLAS = ("res/grass_atlas.png");
+	public static final String TREE_ONE_ATLAS = ("res/tree_one_atlas.png");
+	public static final String TREE_TWO_ATLAS = ("res/tree_two_atlas.png");
+	public static final String GAME_COMPLETED = ("res/game_completed.png");
+	public static final String RAIN_PARTICLE = ("res/rain_particle.png");
+	public static final String WATER_TOP = ("res/water_atlas_animation.png");
+	public static final String WATER_BOTTOM = ("res/water.png");
+	public static final String SHIP = ("res/ship.png");
 
 	public static BufferedImage GetSpriteAtlas(String fileName) {
 		BufferedImage img = null;
@@ -86,11 +86,14 @@ public class LoadSave {
 		File[] filesSorted = new File[files.length];
 
 		for (int i = 0; i < filesSorted.length; i++)
-			for (File file1 : files) {
-                        if (file1.getName().equals((i + 1) + ".png")) {
-                            filesSorted[i] = file1;
-                        }
-                    }
+		{
+			for (File file1 : files)
+			 {
+				if (file1.getName().equals((i + 1) + ".png")) {
+					filesSorted[i] = file1;
+				}
+            }
+		}
 
 		BufferedImage[] imgs = new BufferedImage[filesSorted.length];
 
